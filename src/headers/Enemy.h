315049c19c34101874167a71f7a3dd9fbe3ebb96
zbarek12345 <zbarek12345@gmail.com>
2025-11-23 23:00:36 +0100

    Fixed entity a bit

--- src/headers/Enemy.h
+++ src/headers/Enemy.h
@@ -6,12 +6,46 @@
 
 #include "Entity.h"
 
+struct RotationalAnimation {
+	gl_texture angles[8];
+
+	///Whether Frame [1],[2],[3] is mirrored;
+	bool mirrors[3];
+	double frame_time;
+};
+
+struct EnemyAnimationLoader {
+	std::string base_name;
+	///Idle, Chase, Melee(opt.), Projectile(opt.), Pain, Death, Gibbing (opt.)
+	std::vector<std::string> texture_sequences[7];
+};
+
 class Enemy: public Entity {
-    public:
-    Enemy(svec2 pos, int i, const char * str, const char * text, bool cond, EntityPosType floor);
+	uint16_t health = 100;
+	float painChance = 0.05f;
+	uint16_t speed;
+	double reactionTime;
+
+	Enemy(EnemyAnimationLoader loader,uint16_t health,uint16_t speed,double reactionTime,float painChance);
+	///Idle, Chase, Melee(opt.), Projectile(opt.), Pain, Death, Gibbing (opt.)
+	std::vector<RotationalAnimation> animations[7];
+
+	///TODO - W ten sposób wczytamy animacje ...
+	void LoadAnimations();
+
+	enum class State {
+		Idle,
+		Chasing,
+		Melee,
+		Projectile,
+		Pain,
+		Dying,
+		Dead,
+		Resurrected,
+	};
+
+	public:
 
 };
 
-//todo enemy
-#define Imp(pos) Enemy(pos, 20, "TROO","ABCD", true, EntityPosType::Floor)
 #endif //HERETIC_ENEMY_H
